465406aead17456a4ff7a790ff10d499138b4058
Rohan Malik <rohan_malik@ryecountryday.org>
2022-11-06 17:43:57 -0500

    Add planned routes for some pages

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Home } from "./pages/Home";
+import { Books } from "./pages/Books";
+import { About } from "./pages/About";
+import { Thoughts } from "./pages/Thoughts";
+import { Projects } from "./pages/Projects";
 
 function Router() {
     return (
         <Routes>
             <Route exact path="/" element={<Home />}/>
+            <Route exact path="/books" element={<Books />}/>
+            <Route exact path="/projects" element={<Projects />}/>
+            <Route exact path="/about" element={<About />}/>
+            <Route exact path="/thoughts" element={<Thoughts />}/>
         </Routes>
     )
 }
