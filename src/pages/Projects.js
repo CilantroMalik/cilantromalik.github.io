85811df4387b341679073a149bda01d11d45d6f4
Rohan Malik <rohan_malik@ryecountryday.org>
2022-11-08 10:05:23 -0500

    Add template pages

---
+++ src/pages/Projects.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+export const Projects = () => {
+
+    return (
+        <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+
+        </div>
+    )
+}
